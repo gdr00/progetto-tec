9e763e58cca904d30d1ea30eea11455805287a85
gdr-00 <gabriele.dare00@gmail.com>
2023-01-11 16:10:36 +0100

    added to top button

--- js/main.js
+++ js/main.js
@@ -25,3 +25,18 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].classList.add("active");
 }
+
+var btn = $('#toTopBTN');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
