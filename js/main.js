5eeac9162641e0a07d789a5d538350307e9de1e2
gdr-00 <gabriele.dare00@gmail.com>
2023-01-13 16:06:37 +0100

    refactored js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var slideIndex = 1;
 var theme = "dark";
-var properties = ['--bodybgcolor', '--navbarbg', '--txtcolor', '--linkcolor', '--linkvisited', '--linknavhovercolor', '--iconpos', '--logo']
-var values = [['#050505', '#050505', '#f1f1f1', '#f1f1f1', '#f1f1f1', '#1BDC88', 'left', 'url(../img/logo-sfondo-nero.webp)'], ['#ffffff', '#ffffff', '#000000', '#000000', '#000000', '#1BDC88', 'right', 'url(../img/logo-sfondo-bianco.webp)']]
+var themeProperties = ['--bodybgcolor', '--navbarbg', '--txtcolor', '--linkcolor', '--linkvisited', '--linknavhovercolor', '--iconpos', '--logo']
+var themeValues = [['#050505', '#050505', '#f1f1f1', '#f1f1f1', '#f1f1f1', '#1BDC88', 'left', 'url(../img/logo-sfondo-nero.webp)'], ['#ffffff', '#ffffff', '#000000', '#000000', '#000000', '#1BDC88', 'right', 'url(../img/logo-sfondo-bianco.webp)']]
 
 document.readyState(setup());
 
@@ -48,14 +48,14 @@ function changeTheme() {
 function setTheme(theme) {
   if (theme == 'dark') {
     localStorage.setItem('panelTheme', theme);
-    for (var i = 0; i < properties.length; i++) {
-      document.documentElement.style.setProperty(properties[i], values[0][i]);
+    for (var i = 0; i < themeProperties.length; i++) {
+      document.documentElement.style.setProperty(themeProperties[i], themeValues[0][i]);
     } /* attenzione alla posizione delle variabili globali, prima erano tra set theme e changeTheme, siccome change chiama set e le variabili erano dichiarate sotto change per change non esistevano ancora e crashava con metodo .lenght*/
   }
   if (theme == 'light') {
     localStorage.setItem('panelTheme', theme);
-    for (var i = 0; i < properties.length; i++) {
-      document.documentElement.style.setProperty(properties[i], values[1][i]);
+    for (var i = 0; i < themeProperties.length; i++) {
+      document.documentElement.style.setProperty(themeProperties[i], themeValues[1][i]);
     }/**/
   }
 }
