1f7acd819af08ef4ccbb0e9986bd3986d5937e9d
gdr-00 <gabriele.dare00@gmail.com>
2023-01-25 18:25:53 +0100

    update test

--- html/nocrawling/test.php
+++ html/nocrawling/test.php
@@ -1,8 +1,3 @@
 <?php
-$path_parts = pathinfo('/www/progetto/html/nocrawling/test.php');
-
-echo $path_parts['dirname'], "\n";
-echo $path_parts['basename'], "\n";
-echo $path_parts['extension'], "\n";
-echo $path_parts['filename'], "\n";
+print_r(pathinfo('/www/progetto-tec/html/nocrawling/'));
 ?>
