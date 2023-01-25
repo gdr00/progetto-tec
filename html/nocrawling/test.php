7b021dc13170da4c9842dc0dc30235c1b4e23703
gdr-00 <gabriele.dare00@gmail.com>
2023-01-25 18:19:03 +0100

    updated test

--- html/nocrawling/test.php
+++ html/nocrawling/test.php
@@ -1,1 +1,8 @@
-<?php echo pathinfo('/www/progetto-tec/php/uploads'); ?>
+<?php
+$path_parts = pathinfo('/www/htdocs/inc/lib.inc.php');
+
+echo $path_parts['dirname'], "\n";
+echo $path_parts['basename'], "\n";
+echo $path_parts['extension'], "\n";
+echo $path_parts['filename'], "\n";
+?>
