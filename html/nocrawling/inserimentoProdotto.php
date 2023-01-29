e8163bdadbd4f41704f470c841582ebd0f749c50
gdr-00 <gabriele.dare00@gmail.com>
2023-01-29 23:58:52 +0100

    funziona l'upload da fixare la path nel file html

--- html/nocrawling/inserimentoProdotto.php
+++ html/nocrawling/inserimentoProdotto.php
@@ -50,7 +50,7 @@ if(isset($_POST['inserisci'])){
      // prende il nome del file immagine
      //torno alla root directory del progetto dato che sono in html/nocrawling
     $target_dir = realpath('../../php/uploads/');
-     // $target_file => Contiene il percorso completo del file caricato (es. uploads/immagine.jpg)
+     // $target_file => Contiene il percorso completo del file caricato (es. var/www/progetto-tec/php/uploads/immagine.jpg)
     $target_file = $target_dir . '/' . basename($_FILES["product-image"]["name"]);
      //alt immagine
     $product_image_alt = pulisciInput($_POST['product-image-alt']);
