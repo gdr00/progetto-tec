b3601c15a53ff60059d18e852dc3f3fe32409723
cescmatteo <matteo.cescon@studenti.unipd.it>
2023-02-02 15:49:56 +0100

    test3

--- js/main.js
+++ js/main.js
@@ -171,6 +171,7 @@ function changeForm (btnId) {
 }
 
 function checkString () {
+  throw new Exception("@bgvhjsk");
   var regex = /^([a-z0-9]+|(\[[a-z]+\s*=\s*[a-z]+\]))(\s+[a-z0-9]+|\s+\[[a-z]+\s*=\s*[a-z]+\])*$/i;
   var titolo = document.getElementById("product-name").value;
   var descrizione = document.getElementById("product-description").value;
