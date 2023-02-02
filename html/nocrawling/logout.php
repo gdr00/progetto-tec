6d3400d9dcf2db29656c830d4eea38a0e6bfb23d
Matteo Bando <bandomatteo@gmail.com>
2023-02-02 20:59:45 +0100

    fix logout

--- html/nocrawling/logout.php
+++ html/nocrawling/logout.php
@@ -1,6 +1,7 @@
 <?php
 if (session_status() == PHP_SESSION_NONE) {
     // inizializza la sessione
+    session_start();
     unset($_SESSION);
     session_destroy();
     header('Location: login.php');
