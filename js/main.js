8e1a8ede3b6827168d0019519a370ffc5d9b29e8
cescmatteo <matteo.cescon@studenti.unipd.it>
2023-02-02 15:35:53 +0100

    check per le regex

--- js/main.js
+++ js/main.js
@@ -171,8 +171,14 @@ function changeForm (btnId) {
 }
 
 function checkString () {
+  var regex = '/^([a-z0-9]+|(\[[a-z]+\s*=\s*[a-z]+\]))(\s+[a-z0-9]+|\s+\[[a-z]+\s*=\s*[a-z]+\])*$/i';
   var titolo = document.getElementById("product-name").value;
   var descrizione = document.getElementById("product-description").value;
   var img_alt = document.getElementById("product-image-alt").value;
-  
+  if (stringCorrectness(regex, titolo))
+    console.log("AAAAAAAAAAAA");
+}
+
+function stringCorrectness (pattern, string) {
+  return pattern.test(string);
 }
